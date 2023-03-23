151f390bb269849189bac0295bd95b01cb96b0ba
marjan-n <87913121+marjan-n@users.noreply.github.com>
2023-03-23 19:03:44 -0400

    getting cities

--- international-times.py
+++ international-times.py
@@ -6,4 +6,14 @@
 # 2. enter the city whose timezone you want the times to be
 
 # Outputs
-# 1. 
+# 1. suggested blocks of time that work well
+
+# Extensions
+# 1. adding scores for times of the day
+# 2. exclusion of certain times
+
+numCities = int(input('How many cities would you like to consider? '))
+listCities = []
+for i in range(numCities):
+    line = 'Enter city # ' + str(i) + ": "
+    listCities.append(input(line))
