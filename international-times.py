625ebe63fa459ae31b4b7744b843b9a2786efae7
marjan-n <87913121+marjan-n@users.noreply.github.com>
2023-03-23 19:14:24 -0400

    get timezone function skeleton

--- international-times.py
+++ international-times.py
@@ -13,12 +13,18 @@
 # 1. adding scores for times of the day
 # 2. exclusion of certain times
 
+def getTimezone(city):
+    pass
+
 numCities = int(input('How many cities would you like to consider? '))
 listCities = []
 for i in range(numCities):
     line = 'Enter city # ' + str(i) + ": "
     listCities.append(input(line))
 print(listCities)
+
+print(getTimezone[listCities[0]])
+
 
 # create a list of ones and zeros; ones if it's 9 am - 5 p.m.. 24 hour in UTC time.
 # and then look at intersection point of all the cities.
