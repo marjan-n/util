364fe1a6e32e48a0278366b7d7b79531dd267dfa
marjan-n <87913121+marjan-n@users.noreply.github.com>
2023-03-23 19:13:14 -0400

    ideas for determining ideal times.

--- international-times.py
+++ international-times.py
@@ -3,10 +3,11 @@
 
 # Inputs
 # 1. enter a bunch of cities
-# 2. enter the city whose timezone you want the times to be
+# 2. enter the city whose timezone you want the times to be - no. answer in all timezones.
 
 # Outputs
 # 1. suggested blocks of time that work well
+# 2. whether no time will work well
 
 # Extensions
 # 1. adding scores for times of the day
@@ -17,3 +18,9 @@ listCities = []
 for i in range(numCities):
     line = 'Enter city # ' + str(i) + ": "
     listCities.append(input(line))
+print(listCities)
+
+# create a list of ones and zeros; ones if it's 9 am - 5 p.m.. 24 hour in UTC time.
+# and then look at intersection point of all the cities.
+# at the beginning, define the bounds.
+# and then, define the bound for each city.
