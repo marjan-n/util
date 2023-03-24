d15520adcd2938ead812db018acdf048cc2407a2
marjan-n <87913121+marjan-n@users.noreply.github.com>
2023-03-23 20:04:37 -0400

    getting table from site for toronto only

--- international-times.py
+++ international-times.py
@@ -13,7 +13,18 @@
 # 1. adding scores for times of the day
 # 2. exclusion of certain times
 
+# from selenium import webdriver
+import pandas as pd
+
 def getTimezone(city):
+    url = 'https://www.timeanddate.com/time/zone/canada/toronto'
+    # response = requests.get(url)
+    # soup = BeautifulSoup(response.text, "html.parser")
+    # test = soup.findAll("table", attrs={"class":"table table--left table--inner-borders-rows"})
+    # print(test)
+    all_tables = pd.read_html(url,match='Current Offset')
+    print(all_tables[0][1][2])
+
     pass
 
 numCities = int(input('How many cities would you like to consider? '))
@@ -23,7 +34,7 @@ for i in range(numCities):
     listCities.append(input(line))
 print(listCities)
 
-print(getTimezone[listCities[0]])
+getTimezone(listCities[0])
 
 
 # create a list of ones and zeros; ones if it's 9 am - 5 p.m.. 24 hour in UTC time.
