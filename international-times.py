b430e48ad45211f958204d269bf96e7ce4ba00a9
marjan-n <87913121+marjan-n@users.noreply.github.com>
2023-03-25 10:17:55 -0400

    common working times are being printed
    stop
    quit

--- international-times.py
+++ international-times.py
@@ -18,6 +18,13 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import re
+import numpy as np
+
+def fixTime(time):
+    if time > 24:
+        time -= 24
+        return time
+    return time
 
 def getWorkingHours(offset, startTime=9, endTime=17):
     # go through the ideal working hours of one city
@@ -28,13 +35,30 @@ def getWorkingHours(offset, startTime=9, endTime=17):
     idealWorkingHours = []
     for i in range(24):
         sum = i+offset
-        if sum > 24:
-            sum -= 24
+        sum = fixTime(sum)
         if sum >= startTime and sum < endTime:
             idealWorkingHours.append(1)
         else:
             idealWorkingHours.append(0)
     return idealWorkingHours
+
+def getCommonWorkingHours(startingTimezoneHoursBinaryArray, timezoneOffsets, startTime = 9, endTime = 17):
+    
+    # there was only ever 1 city inputted. we have ideal working hours
+    if len(timezoneOffsets) < 2:
+        return startingTimezoneHoursBinaryArray
+    
+    dummyCopy = startingTimezoneHoursBinaryArray
+    for offset in timezoneOffsets[1:]:
+        for i in range(24):
+            time = i + offset
+            time = fixTime(time)
+            # if ideal time in starting timezone but not in the 'offset' timezone
+            if startingTimezoneHoursBinaryArray[i] == 1 and (time < startTime or  time > endTime):
+                dummyCopy[i] = 0
+                if np.sum(dummyCopy) == 0:
+                    return [0]*24
+    return dummyCopy
     
 def checkPos(sign):
     if sign == '+':
@@ -62,14 +86,33 @@ for i in range(numCities):
     listCountries.append(input(line))
     timezone = getTimezone(listCities[i], listCountries[i])
     listTimezones.append(timezone)
-    print(getWorkingHours(timezone))
+    # print(getWorkingHours(timezone))
 print()
 print(listCities)
 print(listTimezones)
 
+def translateCommonWorkingHours(referenceTimezoneOffset, commonWorkingHoursBinaries):
+    for i in range(24):
+       # requires translation
+       afterNoon = False
+       if commonWorkingHoursBinaries[i] == 1:
+           time = referenceTimezoneOffset + i
+           time = fixTime(time)
+           if time == 12:
+               print(f'12 p.m.')
+           elif time > 12:
+               print(f'{time-12} p.m.')
+           else:
+               print(f'{time} a.m.')
+               
+
+timezoneBinaries0 = getWorkingHours(listTimezones[0], startTime=9, endTime=17)
+commonWorkingHoursBinaries = getCommonWorkingHours(timezoneBinaries0, listTimezones)
+translateCommonWorkingHours(listTimezones[0], commonWorkingHoursBinaries)
 
 
 # create a list of ones and zeros; ones if it's 9 am - 5 p.m.. 24 hour in UTC time.
 # and then look at intersection point of all the cities.
 # at the beginning, define the bounds.
 # and then, define the bound for each city.
+# still need to remove spaces from city names e.g. new york
